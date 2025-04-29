432df17eed9bbf42dcaabd0a16f800f76d80d10c
Wallace Honorio <wallace.ho@hotmail.com>
2025-04-29 18:05:41 -0300

    Hello World commit.

---
+++ src/test/java/com/walla_ho/med/voll/apiREST/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walla_ho.med.voll.apiREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
